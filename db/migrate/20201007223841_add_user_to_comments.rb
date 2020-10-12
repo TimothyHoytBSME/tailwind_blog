17e83ce7f254fba1b5743b8b630acc433d86054e
Tim Hoyt <timothyhoytbsme@gmail.com>
2020-10-12 12:37:49 -0500

    migrateoops

--- db/migrate/20201007223841_add_user_to_comments.rb
+++ db/migrate/20201007223841_add_user_to_comments.rb
@@ -1,6 +1,6 @@
 class AddUserToComments < ActiveRecord::Migration[6.0]
   def change
     add_reference :comments, :user, foreign_key: true
-    Comment.update_all(user_id: 3 )
+    #Comment.update_all(user_id: 3 )
   end
 end
